d71297a324015b882dae5dc5b76d1f4bfc85214f
Javier Alarcon <alarcon.vt@gmail.com>
2020-07-18 12:55:57 -0500

    implementacion del provider como notificador de cambios de estado

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class TabsPage extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _Paginas(),
-      bottomNavigationBar: _Navegacion(),
+    return ChangeNotifierProvider(
+      create: (_)=> new _NavegacionModel(), //creacion de la instancia del provider (se necesita el widget ChangeNotifierProvider)
+      child: Scaffold(
+        body: _Paginas(),
+        bottomNavigationBar: _Navegacion(),
+      ),
     );
   }
 }
@@ -15,8 +19,13 @@ class _Navegacion extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+
+    //obtencion del provider en un hijo
+    final navegacionModel = Provider.of<_NavegacionModel>(context);
+
     return BottomNavigationBar(
-      currentIndex: 0,
+      currentIndex: navegacionModel.paginaActual,
+      onTap: (i) => navegacionModel.paginaActual = i,
       items: [
         BottomNavigationBarItem( icon: Icon( Icons.person_outline), title: Text('Para ti') ),
         BottomNavigationBarItem( icon: Icon( Icons.public), title: Text('Encabezado') )
@@ -44,4 +53,19 @@ class _Paginas extends StatelessWidget {
       ],
     );
   }
+}
+
+//implementacion de un provider de tipo notificador
+class _NavegacionModel with ChangeNotifier{
+
+  int _paginaActual = 0;
+
+  int get paginaActual => this._paginaActual;
+
+  set paginaActual (int valor) {
+    this._paginaActual = valor;
+    notifyListeners();
+    // ???
+  }
+
 }
